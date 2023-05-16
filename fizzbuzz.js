d634ace014c890afc3b0e066475ea14d306a63d9
ctokoli <matumasie@gamil.com>
2023-05-16 07:20:40 +0000

    coding review

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
-function fizzBuzz(n) {
-    for(let i = 1; i <= n; i++ ){
+function fizzBuzz(a) {
+    for(let i = 1; i <= a; i++ ){
         if( i % 3 == 0 ){
             console.log("Fuzz");
         }else if(i % 5 == 0){
